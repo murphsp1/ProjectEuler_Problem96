b278eb36fcb3d6de5d5f985910793648ed43a395
Sean Murphy <murphsp1@gmail.com>
2014-02-20 14:43:31 -0500

    some more helper functions

--- attempt1.py
+++ attempt1.py
@@ -2,18 +2,29 @@ import numpy as np
 import sys
 
 
-
+BOARD_SIZE = 9
+VALID = np.arange(1,10)
 
 class SodukuBoard:
-    BOARD_SIZE = 9
-    VALID = np.arrange(1,10)
 
-    board_np = np.zeros([BOARD_SIZE, BOARD_SIZE])
+    def __init__(self):
+        self.board_np = np.zeros([BOARD_SIZE, BOARD_SIZE])
+        self.board = [[0] * BOARD_SIZE ] * BOARD_SIZE
+        self.ref_board = []
+        self.ref_board = self.read_boards_from_txt()
 
-    board = [[0] * BOARD_SIZE ] * BOARD_SIZE
 
-    ref_board = []
+    def __str__(self):
+        return self.board_np
 
+    def __repl__(self):
+        return self.board_np
+
+    def check_rows(self):
+        for i in xrange(BOARD_SIZE):
+            if len(np.unique(self.board_np[i,:])) < BOARD_SIZE:
+                return False
+        return True
 
 
     def read_boards_from_txt(self):
@@ -35,29 +46,35 @@ class SodukuBoard:
 
             self.ref_board = ref_board
             self.ref_board_np = np.array(ref_board)
+        return True
 
 
-    def check_rows(board_np):
+
+    def check_columns(self):
         for i in xrange(BOARD_SIZE):
-            if len(np.unique(board_np[i,:])) < BOARD_SIZE:
+            if len(np.unique(self.board_np[:,i])) < BOARD_SIZE:
                 return False
         return True
 
-    def check_columns(board_np):
-        for i in xrange(BOARD_SIZE):
-            if len(np.unique(board_np[:,i])) < BOARD_SIZE:
-                return False
+    def check_sub_boards(self):
+
+        for r in range(3): #rows
+            for c in range(3): #columns
+                col = c*3
+                row = r*3
+                sub_board = self.board_np[row:row+3, col:col+3]
+                print(sub_board)
+                if len(np.unique(sub_board)) < BOARD_SIZE:
+                    return False
         return True
 
-    def check_sub_boards(board_np)
-        #need to run through each subboard, there are nine ... and
 
-
-    def validate_solution(board_np):
-        if check_rows(board_np):
-            if check_columns(board_np):
-                if check_sub_boards(board_np):
+    def validate_solution(self):
+        if self.check_rows(self.board_np):
+            if self.check_columns(board_np):
+                if self.check_sub_boards(board_np):
                     return True
+        return False
 
 
 
